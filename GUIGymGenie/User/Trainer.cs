79d205fdf58982375985a263002d20321ca2c697
nkaur <navroop.kaur@wsu.edu>
2020-05-03 15:36:06 -0700

    changes

--- GUIGymGenie/User/Trainer.cs
+++ GUIGymGenie/User/Trainer.cs
@@ -7,14 +7,19 @@ namespace User
     [Serializable]
     class Trainer : _User
     {
+
         public Trainer(int id, string name, string email, string password) : base(id, name, email, password)
         {
             Role = 1;
         }
+
 
         public override string ToString()
         {
             return base.ToString() + " |";
         }
     }
+
+   
+
 }
